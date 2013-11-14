c4e6b74f9bcc8e94914993ab8f247e0aa639ba03
Fmelle <fridtjof.melle@gmail.com>
2013-11-14 19:26:13 +0100

    Fixed commande voiles header..

--- APP/commandevoiles.c
+++ APP/commandevoiles.c
@@ -5,34 +5,20 @@
 #include "../applications/servomoteur.h"
 #include "../applications/girouette.h"
 
-//Fonction qui initialise toutes les fonctions necessaire a la commande des voiles
-void Init_Commandevoiles( void ) {
-	
+void Init_Commande_Voiles(void) {
+	// Vars internes
+	float Period_Set;
+
 	//Initialisation girouette
 	Init_Girouette();
 	Port_IO_Init_Alternative_Output(GPIOB, 8);
 	
-	float Period_Set, Period_PWM, Perc_Cycle;
-	
 	//Initialisation du TIM1 en PWM
 	Period_Set = Timer_PWM_Init(TIM1, 20000, 1);
-	
-	//Mise a 1 du bit MOE
-	TIM1->BDTR |= 0x8000;
-	
 }
 
-//Fonction qui lance la commande des voiles
-void Deb_Commande_Voiles (void){
-	
-//Lancement du programme de commande de voile
-	while (1) {
-		
+void Commande_Voiles(void) {
+	float Period_PWM, Perc_Cycle;
 	Perc_Cycle = Calcule_Percentage_Angle(Return_Angle_Girouette());
 	Period_PWM = Timer_PWM_Set_Duration(TIM1, Perc_Cycle, 1);
-	
-	}
-	
-	
-	
 }
--- APP/commandevoiles.h
+++ APP/commandevoiles.h
@@ -1,4 +1,22 @@
+#ifndef _COMMANDE_VOILES_H__
+#define _COMMANDE_VOILES_H__
+
+/**
+* Initialisation des toutes les fonctions necessaire a la 
+* commande des voiles
+* @param void
+* @return void
+**/
+void Init_Commande_Voiles(void);
+
+/**
+* A tourner tous les cycles pour la commande
+* des voiles
+* @param void
+* @return void
+**/
+void Commande_Voiles(void);
+
+#endif /* _COMMANDE_VOILES_H__ */
 
 
-//Fonction qui initialise toutes les fonctions necessaire a la commande des voiles
-void Init_Commandevoiles( void )
